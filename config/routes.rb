520201c496c822149138380250fa5c41c625e209
otisgordon <otis@uchicago.edu>
2022-03-18 21:50:57 +0000

    Changed the link to spotify

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/auth/spotify/callback', to: 'spotify_accounts#spotify'
   get '/callback', to: 'spotify_accounts#spotify'
   get("/options", { :controller => "spotify_accounts", :action => "options" })
-  get("/test", { :controller => "spotify_accounts", :action => "test" })
+  get("/home", { :controller => "spotify_accounts", :action => "test" })
   get("/scrape", { :controller => "movies", :action => "scrape" })
 
 
